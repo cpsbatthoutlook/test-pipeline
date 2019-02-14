04f0ec3c0f5c103cbff350c22b0a4c742cdfb79d
Your Name <you@example.com>
2019-02-14 03:41:00 +0000

    adfaf

--- CreateDockerEnv/centosImage/dockerfile.sh
+++ CreateDockerEnv/centosImage/dockerfile.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
-
+#Starting
+echo "Looking at the contents of /app"
+find /app
+sleep 10
 yum -y update; yum clean all
-
 #Install prerequisite
-yum -y install openssh-server passwd git build-essential git python-dev python-pip  centos-release-scl rh-python36
+yum -y install epel-release
+yum -y install openssh-server passwd git build-essential git 
+echo "Installing Python PIP"
+yum -y install python-dev python-pip  
+echo "Installing Python3 "
+yum -y install centos-release-scl rh-python36
+echo "Running PIP commands"
+sleep 5
 pip install --upgrade pip
 pip install requests #GCP Authentication
-pip install  googleauth
+pip install  google-auth
 export ANSIBLE_HOST_KEY_CHECKING=False ##optional
 # Python 3
 scl enable rh-python36 bash 
+#Ansible installation
 yum -y install ansible
-
+#
 ##Create aliases
 cat <<EOF >> ~/.bashrc
 alias ans="ansible "
@@ -19,16 +29,23 @@ alias ansd="ansible-doc "
 alias ansg="ansible-galaxy "
 alias ansp="ansible-playbook  "
 EOF
-
 ##
-yum clean all
-
-#Copy the /root/.ssh over with authorized_keys
+echo "Moving the SSH files over to /root"
 mv /app/.ssh /root
+find /root/.ssh
 chmod 600 /root/.ssh/*
 chmod 750 /root/.ssh
+ls -l /root/.ssh
+sleep 5
 ##
 #Copy the /etc/ssh files over to this place
-(cd / && tar xvf /app/centosImage.tar)
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' 
+echo "Untarring the ssh files"
+#ls -l /app/centosImage.tar
+#(cd / && tar xvf /app/centosImage/centosImage.tar)
+(cd /app && rm -rf /etc/ssh && mv etc/ssh /etc)
+find /etc/ssh
+sleep 5
 ##Start SSH
 /usr/sbin/sshd -D &
+
